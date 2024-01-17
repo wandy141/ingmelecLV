30ba83e1661c5a125d0f08cc4d56df53335b0d57
wandy141 <115728722+wandy141@users.noreply.github.com>
2024-01-17 11:39:10 -0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,7 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'dist', // Esta es la ruta deseada para los archivos construidos
+      },
 });
